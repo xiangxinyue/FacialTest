eedfeca3afb89aca9737f33ba82fe67d10bb5c04
Kay Song <kaysong@Kays-Mac-mini.local>
2020-09-15 11:31:25 -0600

    add timer

--- client/src/components/test.jsx
+++ client/src/components/test.jsx
@@ -17,25 +17,61 @@ class Test extends React.Component {
         "008.jpg",
         "009.jpg",
       ],
+      time: 0,
+      timer: [],
     };
   }
 
+  startTimer = () => {
+    this.timer = setInterval(
+      () =>
+        this.setState({
+          time: this.state.time + 0.01,
+        }),
+      10
+    );
+    console.log("start");
+  };
+  stopTimer = () => {
+    clearInterval(this.timer);
+    console.log("stop");
+  };
+  resetTimer = () => {
+    this.setState({ time: 0 });
+    console.log("reset");
+  };
+
+  componentDidMount = () => {
+    this.startTimer();
+  }
+
   handleZClick = () => {
-    const { index, pictures } = this.state;
+    const { index, pictures, timer } = this.state;
+    this.stopTimer();
+    timer.push(Number(this.state.time).toFixed(2));
+    this.resetTimer();
+    this.setState({ timer });
     if (pictures.length > index + 1) {
-      this.setState({ index: index + 1 });
+      this.startTimer();
     }
+    this.setState({ index: index + 1 });
   };
 
   handleMClick = () => {
-    const { index, pictures } = this.state;
+    const { index, pictures, timer } = this.state;
+    this.stopTimer();
+    timer.push(Number(this.state.time).toFixed(2));
+    this.resetTimer();
+    this.setState({ timer });
     if (pictures.length > index + 1) {
-      this.setState({ index: index + 1 });
+      this.startTimer();
     }
+    this.setState({ index: index + 1 });
   };
 
   render() {
     const { pictures, index } = this.state;
+    console.log(this.state.timer);
     return (
       <div
         style={{
@@ -44,34 +80,42 @@ class Test extends React.Component {
           marginTop: "5%",
         }}
       >
-        <h4>
-          Stage {index + 1} of {pictures.length}
-        </h4>
-        <img
-          src={process.env.PUBLIC_URL + "/pictures/" + pictures[index]}
-          style={{ width: "20%" }}
-        />
-        <div className="row" style={{ marginTop: "30%" }}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={this.handleZClick}
-          >
-            <h2>Z</h2>
-            <br />
-            <br />a person without facial disfigurement
+        {
+          index < pictures.length ?
+            <div>
+              <h4>
+                Stage {index + 1} of {pictures.length}
+              </h4>
+              <img
+                src={process.env.PUBLIC_URL + "/pictures/" + pictures[index]}
+                style={{ width: "20%" }}
+              />
+              <div className="row" style={{ marginTop: "30%" }}>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={this.handleZClick}
+                >
+                  <h2>Z</h2>
+                  <br />
+                  <br />a person without facial disfigurement
           </Button>
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ marginLeft: "50%", marginRight: 0, position: "fixed" }}
-            onClick={this.handleMClick}
-          >
-            <h2>M</h2>
-            <br />
-            <br />a person with facial disfigurement
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ marginLeft: "50%", marginRight: 0, position: "fixed" }}
+                  onClick={this.handleMClick}
+                >
+                  <h2>M</h2>
+                  <br />
+                  <br />a person with facial disfigurement
           </Button>
-        </div>
+              </div>
+            </div>
+            :
+            <h2>Well Done!</h2>
+        }
+
       </div>
     );
   }
--- index.js
+++ index.js
@@ -2,39 +2,39 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
-const keys = require("./config/keys");
+// const keys = require("./config/keys");
 
 const { json } = require("body-parser");
 app.use(json());
 
-mongoose
-  .connect(keys.mongoURI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  })
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(keys.mongoURI, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//   })
+//   .catch((err) => console.log(err));
 
-require("./models/data");
-const Data = mongoose.model("datas");
+// require("./models/data");
+// const Data = mongoose.model("datas");
 
-app.post("/api/data", async (req, res) => {
-  console.log(req.body);
-  await new Data({
-    date: new Date().toLocaleString(),
-    ...req.body,
-  }).save();
-  res.status(201).send({});
-});
+// app.post("/api/data", async (req, res) => {
+//   console.log(req.body);
+//   await new Data({
+//     date: new Date().toLocaleString(),
+//     ...req.body,
+//   }).save();
+//   res.status(201).send({});
+// });
 
-app.get("/api/data", async (req, res) => {
-  const data = await Data.find();
-  res.status(200).send(data);
-});
+// app.get("/api/data", async (req, res) => {
+//   const data = await Data.find();
+//   res.status(200).send(data);
+// });
 
-app.delete("/api/data/:id", async (req, res) => {
-  await Data.findByIdAndDelete(req.params.id);
-  res.send({});
-});
+// app.delete("/api/data/:id", async (req, res) => {
+//   await Data.findByIdAndDelete(req.params.id);
+//   res.send({});
+// });
 
 if (["production", "ci"].includes(process.env.NODE_ENV)) {
   app.use(express.static("client/build"));
